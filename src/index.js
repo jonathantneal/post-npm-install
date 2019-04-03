ae594f31fdf1fd2a8942b52873291b3c49f73da2
Jonathan Yee <jonyeezs@gmail.com>
2019-04-04 00:36:54 +1000

    Add option to use npm ci to install and only when ci rules is satisfy else fall back to npm install

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import { exec as callbackExec, execSync } from 'child_process';
+import { access, constants as fsconstants } from "fs";
 
-export default function postNpmInstall() {
+export default function postNpmInstall(useCi = false) {
+	checkChanges()
+	.then(haveDepsChanged => {
+		if (haveDepsChanged) {
+			// figure out if we should use ci command
+			return canUseCi(useCi);
+		} else {
+			// exit early if no changes
+			process.exit(0);
+		}
+	}) 
+	.then(useCi => {
+		execSync(useCi ? 'npm ci' : 'npm install', { stdio: ['pipe', 'pipe', process.stderr] })
+		
+		console.log('Please restart the server.')
+	})
+	.then(
+		() => process.exit(0),
+		error => {
+			console.error(error);
+			
+			process.exit(1);
+		});
+	}
+	
 	// read the current and last package.json files
-	Promise.all([
+const checkChanges = () => Promise.all(
+	[
 		exec('git show ORIG_HEAD:package.json'),
 		exec('git show HEAD:package.json')
 	]).then(pkgs => {
@@ -10,31 +36,29 @@ export default function postNpmInstall() {
 		const pkg1 = JSON.parse(pkgs[0])
 		const pkg2 = JSON.parse(pkgs[1])
 
-		// conditionally run `npm install` if package dependencies have changed
-		const haveDepsChanged =
-			haveObjectValuesChanged(pkg1.dependencies, pkg2.dependencies) ||
-			haveObjectValuesChanged(pkg1.devDependencies, pkg2.devDependencies)
-
-		if (haveDepsChanged) {
-			execSync('npm install', { stdio: ['pipe', 'pipe', process.stderr] })
-
-			console.log('Please restart the server.')
-		}
+		// conditionally run install if package dependencies have changed
+		return haveObjectValuesChanged(pkg1.dependencies, pkg2.dependencies) ||
+		haveObjectValuesChanged(pkg1.devDependencies, pkg2.devDependencies)
 	}, error => {
 		const catchableError = /^fatal: Invalid object name/;
-
 		if (!catchableError.test(error)) {
 			throw new Error(catchableError);
 		}
-	}).then(
-		() => process.exit(0),
-		error => {
-			console.error(error);
+	});
 
-			process.exit(1);
+const canUseCi = (optForCi) => Promise.all([
+		checkNpmCiExist(),
+		doesPackageJsonExist()
+	])
+	.then(([hasCiCmd, hasLock]) => {
+		if (!hasLock && optForCi) { 
+			console.warn(`
+Please ensure you have a lockfile before attempting to use npm ci. 
+Falling back to npm install.`);
 		}
-	);
-}
+		
+		return hasCiCmd && hasLock && optForCi;
+	});
 
 // promise-ified exec
 const exec = (cwd, opts) => new Promise((resolve, reject) => {
@@ -45,6 +69,15 @@ const exec = (cwd, opts) => new Promise((resolve, reject) => {
 			resolve(stdout)
 		}
 	})
+});
+
+const checkNpmCiExist = () => exec('npm ci --help')
+.then(output => output.toString() === 'npm ci');
+
+const doesPackageJsonExist = () => new Promise((resolve) => {
+	access('package-lock.json', fsconstants.R_OK, (err) => {
+		resolve(!err);
+	});
 });
 
 // return whether the values of two objects differ
